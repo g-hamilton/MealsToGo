28d3c18cbd4f7cc459886f8c2280b90a63c2c8de
Greg Hamilton <greghamilton1@gmail.com>
2021-05-16 13:45:18 +0100

    started basic ui layout within safe area ios

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Our React Native blank canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ backgroundColor: 'green' }}>
+          <Text style={{ padding: 16 }}>search</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: 'blue' }}>
+          <Text style={{ padding: 16 }}>list</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style='auto' />
+    </>
   );
 }
 

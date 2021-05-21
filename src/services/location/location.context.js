2bcfb3a9650658123581bfeb7244843907030e8d
g-hamilton <greghamilton1@gmail.com>
2021-05-21 11:53:16 +0100

    fixed anti-pattern

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 
 import { locationRequest, locationTransform } from "./location.service";
 
@@ -11,13 +11,16 @@ export const LocationContextProvider = ({ children }) => {
   const [error, setError] = useState(null);
 
   const onSearch = (searchKeyword) => {
-    if (!searchKeyword.length) {
+    setIsLoading(true);
+    setKeyword(searchKeyword);
+  };
+
+  useEffect(() => {
+    if (!keyword.length) {
       // don't do anything
       return;
     }
-    setIsLoading(true);
-    setKeyword(searchKeyword);
-    locationRequest(searchKeyword.toLowerCase())
+    locationRequest(keyword.toLowerCase())
       .then(locationTransform)
       .then((result) => {
         setIsLoading(false);
@@ -27,7 +30,7 @@ export const LocationContextProvider = ({ children }) => {
         setIsLoading(false);
         setError(err);
       });
-  };
+  }, [keyword]);
 
   return (
     <LocationContext.Provider
